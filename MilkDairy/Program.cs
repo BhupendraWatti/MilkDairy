763cc1b87e988cf9ff12929f0c87eaf21cad7a8c
BhupendraWatti <bhupendra2422006watti@gmail.com>
2025-04-10 18:47:13 +0530

    Past 3 months changes like identity etc.

--- MilkDairy/Program.cs
+++ MilkDairy/Program.cs
@@ -10,7 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
